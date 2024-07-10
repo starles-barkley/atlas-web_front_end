23ce0f85bc38eec77bb1d421bbfe683d55b00874
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-07-10 22:56:37 +0000

    create prime  number script

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,29 @@
+(function() {
+  function countPrimeNumbers() {
+      let primeCount = 0;
+
+      for (let num = 2; num <= 100; num++) {
+          let isPrimeNumber = true;
+          for (let divisor = 2; divisor <= Math.sqrt(num); divisor++) {
+              if (num % divisor === 0) {
+                  isPrimeNumber = false;
+                  break;
+              }
+          }
+          if (isPrimeNumber) {
+              primeCount++;
+          }
+      }
+
+      return primeCount;
+  }
+
+  const startTime = performance.now();
+  setTimeout(() => {
+      for (let i = 0; i < 100; i++) {
+          countPrimeNumbers();
+      }
+      const endTime = performance.now();
+      console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
+  }, 0);
+})();
